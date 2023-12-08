5c62a6367efec6178980a6ee62510d19fce29dcd
faritdev <albavi.farit2@gmail.com>
2023-12-08 02:42:52 -0300

    changes

--- src/main/java/com/timeRegisterAPI/TimeRegisterAPI/TimeRegisterApiApplication.java
+++ src/main/java/com/timeRegisterAPI/TimeRegisterAPI/TimeRegisterApiApplication.java
@@ -3,11 +3,14 @@ package com.timeRegisterAPI.TimeRegisterAPI;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class TimeRegisterApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TimeRegisterApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TimeRegisterApiApplication.class, args);
+
+    }
+
 
 }
